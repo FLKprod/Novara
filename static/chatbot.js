f982e6ba9e928ae787055bcd42cde29bae1ed879
Winner <126358343+Sclervil@users.noreply.github.com>
2024-06-21 02:15:36 -0400

    Update chatbot.js

--- static/chatbot.js
+++ static/chatbot.js
@@ -20,40 +20,39 @@ function toggleChatbot() {
     }
 }
 
-async function sendMessage() {
+var socket = io();
+var currentParagraph = null;
+
+socket.on('connect', function() {
+    console.log('Connected to the server.');
+});
+
+socket.on('message_from_server', function(data) {
+    const chatbotMessages = document.getElementById('chatbotMessages');
+    if (!currentParagraph && data.text != '') {
+        currentParagraph = document.createElement("p");
+        currentParagraph.innerHTML = `<strong style="color:#460F9B">VeriFile: </strong>`;
+        document.getElementById('chatbotMessages').appendChild(currentParagraph);
+    }
+
+    if (!data.more) {
+        currentParagraph = null; // Reset for next message
+    } else{
+        currentParagraph.innerHTML += data.text;
+        chatbotMessages.scrollTop = chatbotMessages.scrollHeight;
+    }
+});
+
+function sendMessage() {
     const userInput = document.getElementById('userInput').value;
     if (userInput.trim() === '') return;
     document.getElementById('userInput').value = '';
 
     displayMessage('Utilisateur', userInput);
 
-    const headers = {
-        'Content-Type': 'application/json'
-    };
-
-    const body = {
-        message: userInput
-    };
-
-    try {
-        const response = await fetch('/sendMessage', {
-            method: 'POST',
-            headers: headers,
-            body: JSON.stringify(body)
-        });
-
-        if (!response.ok) {
-            throw new Error(`Erreur HTTP! statut: ${response.status}`);
-        }
-
-        const data = await response.json();
-        const botResponse = data.response;
-        displayMessage('VeriFile', botResponse);
-    } catch (error) {
-        console.error('Erreur:', error);
-        displayMessage('VeriFile', 'Désolé, une erreur est survenue.');
-    }
+    socket.emit('message_from_client', { message: userInput });
 }
+
 
 function displayMessage(type, message) {
     const chatbotMessages = document.getElementById('chatbotMessages');
